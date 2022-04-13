e58d7d35ca02a8cb1a05c3a33da80ecb40642c91
T-Visor <turkimbro@yahoo.com>
2022-04-13 12:36:07 -0400

    finalizing code on training a model

--- transfer-learning.py
+++ transfer-learning.py
@@ -2,6 +2,8 @@ import tensorflow
 from tensorflow.keras.models import  Model
 from tensorflow.keras.layers import Flatten, Dense, Input
 from keras_vggface.vggface import VGGFace
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
 
 #custom parameters
 nb_class = 2
@@ -15,5 +17,27 @@ x = Dense(hidden_dim, activation='relu', name='fc7')(x)
 out = Dense(nb_class, activation='softmax', name='fc8')(x)
 custom_vgg_model = Model(vgg_model.input, out)
 
-# Train your model as usual.
-# ...
+custom_vgg_model.compile(optimizer='adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+
+# Train your model as usual-------------------------------------------
+# create data generator
+datagen = ImageDataGenerator(rotation_range=30, rescale=1.0/255.0)
+
+dataset = 'cropped/images'
+
+# prepare iterators
+train_it = datagen.flow_from_directory(dataset,
+                                        class_mode='categorical', 
+                                        batch_size=64, 
+                                        target_size=(200, 200))
+
+test_it = datagen.flow_from_directory(dataset,
+                                      class_mode='categorical', 
+                                      batch_size=64, 
+                                      target_size=(200, 200))
+
+# fit model
+history = custom_vgg_model.fit_generator(train_it, steps_per_epoch=len(train_it),
+                              validation_data=test_it, validation_steps=len(test_it), epochs=60,
+                              verbose=1)
+
