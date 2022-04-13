b0d254a5197b42c14a6efb912a0eb0f111439bdf
T-Visor <turkimbro@yahoo.com>
2022-04-13 13:59:56 -0400

    switched to RESNET model, need to find a way to fix the high validation loss values

--- transfer-learning.py
+++ transfer-learning.py
@@ -1,23 +1,27 @@
 import tensorflow
 from tensorflow.keras.models import  Model
 from tensorflow.keras.layers import Flatten, Dense, Input
-from keras_vggface.vggface import VGGFace
+from keras_vggface.vggface import VGGFace, RESNET50
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 
 #custom parameters
+# NOTE: make sure the number of classes are consistent with the 
+#       amount found by image data generators
+#
+#       Also, input_shape parameters need to match
 nb_class = 7
 hidden_dim = 512
 
-vgg_model = VGGFace(include_top=False, input_shape=(200, 200, 3))
-last_layer = vgg_model.get_layer('pool5').output
+base_model = RESNET50(include_top=False, input_shape=(200, 200, 3))
+last_layer = base_model.get_layer('avg_pool').output
 x = Flatten(name='flatten')(last_layer)
 x = Dense(hidden_dim, activation='relu', name='fc6')(x)
 x = Dense(hidden_dim, activation='relu', name='fc7')(x)
 out = Dense(nb_class, activation='softmax', name='fc8')(x)
-custom_vgg_model = Model(vgg_model.input, out)
+custom_model = Model(base_model.input, out)
 
-custom_vgg_model.compile(optimizer='adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+custom_model.compile(optimizer='adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
 
 # Train your model as usual-------------------------------------------
 # create data generator
@@ -28,16 +32,16 @@ dataset = 'cropped/images'
 # prepare iterators
 train_it = datagen.flow_from_directory(dataset,
                                         class_mode='categorical', 
-                                        batch_size=16, 
+                                        batch_size=32, 
                                         target_size=(200, 200))
 
 test_it = datagen.flow_from_directory(dataset,
                                       class_mode='categorical', 
-                                      batch_size=16, 
+                                      batch_size=32, 
                                       target_size=(200, 200))
 
 # fit model
-history = custom_vgg_model.fit_generator(train_it, steps_per_epoch=len(train_it),
+history = custom_model.fit_generator(train_it, steps_per_epoch=len(train_it),
                               validation_data=test_it, validation_steps=len(test_it), epochs=60,
                               verbose=1)
 
